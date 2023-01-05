784c10acb0f875dfe23af3785695c8cf1f27cf9c
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-05 15:55:29 +0300

    Json Object

---
+++ Consumer/src/main/java/com/works/props/Message.java
@@ -0,0 +1,12 @@
+package com.works.props;
+
+import lombok.Data;
+
+@Data
+public class Message {
+
+    private Integer id;
+    private String name;
+    private String text;
+
+}
--- Consumer/src/main/java/com/works/services/ConsumerService.java
+++ Consumer/src/main/java/com/works/services/ConsumerService.java
@@ -1,18 +1,27 @@
 package com.works.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.works.props.Message;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ConsumerService {
+
+    final ObjectMapper objectMapper;
 
     // @Value("${spring.kafka.producer.topic}")
     // private String topicName;
 
     @KafkaListener(topics = "${spring.kafka.consumer.topic}", groupId = "${spring.kafka.consumer.group-id}" )
-    public void listen( String message ) {
-        System.out.println( message );
+    public void listen( String message ) throws JsonProcessingException {
+        Message messageObj = objectMapper.readValue(message, Message.class);
+        System.out.println( messageObj.getId() );
+        System.out.println( messageObj.getName() );
+        System.out.println( messageObj.getText() );
     }
 
 }
